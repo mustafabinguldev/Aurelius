4dbf6410da2bfa65a78c67ecc754ea6585e2abaf
Mustafa Bingül <85060597+BingulHan@users.noreply.github.com>
2025-03-06 18:50:49 +0300

    Implement PUT and DELETE methods for resource management

--- src/main/java/tech/bingulhan/webserver/response/impl/restful/RestFulRequestType.java
+++ src/main/java/tech/bingulhan/webserver/response/impl/restful/RestFulRequestType.java
@@ -1,12 +1,14 @@
 package tech.bingulhan.webserver.response.impl.restful;
 
 import lombok.Getter;
+import tech.bingulhan.webserver.response.impl.restful.impl.DeleteRestFulResponseHandler;
 import tech.bingulhan.webserver.response.impl.restful.impl.GetRestFulResponseHandler;
 import tech.bingulhan.webserver.response.impl.restful.impl.PostRestFulResponseHandler;
+import tech.bingulhan.webserver.response.impl.restful.impl.PutRestFulResponseHandler;
 
 public enum RestFulRequestType {
 
-    GET(new GetRestFulResponseHandler()), POST(new PostRestFulResponseHandler());
+    GET(new GetRestFulResponseHandler()), POST(new PostRestFulResponseHandler()), DELETE(new DeleteRestFulResponseHandler()), PUT(new PutRestFulResponseHandler());
 
     @Getter
     private RestFulResponseHandler handler;
---
+++ src/main/java/tech/bingulhan/webserver/response/impl/restful/impl/DeleteRestFulResponseHandler.java
@@ -0,0 +1,80 @@
+package tech.bingulhan.webserver.response.impl.restful.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+import tech.bingulhan.webserver.app.restful.RestFulResponseHelper;
+import tech.bingulhan.webserver.app.restful.RestFulResponseStructure;
+import tech.bingulhan.webserver.response.NettyResponseService;
+import tech.bingulhan.webserver.response.RequestStructure;
+import tech.bingulhan.webserver.response.impl.restful.RestFulResponseHandler;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+
+public class DeleteRestFulResponseHandler implements RestFulResponseHandler {
+
+
+
+    @Override
+    public void handle(NettyResponseService service, RequestStructure structure, RestFulResponseStructure responseStructure) {
+
+        FullHttpRequest request = service.getRequest();
+        String bodyJson = request.content().toString(StandardCharsets.UTF_8);
+        Object o = null;
+
+        String cookieHeader = request.headers().get(HttpHeaders.Names.COOKIE);
+        HashMap<String, String> receivedCookies = new HashMap<>();
+        if (cookieHeader!=null) {
+            Iterable<Cookie> cookies = CookieDecoder.decode(cookieHeader);
+            cookies.forEach(cookie -> {
+                receivedCookies.put(cookie.getName(), cookie.getValue());
+            });
+        }
+        try {
+            o = responseStructure.getRestFulResponse().convert(bodyJson);
+            FullHttpResponse response = new DefaultFullHttpResponse(
+                    HttpVersion.HTTP_1_1, HttpResponseStatus.OK,
+                    Unpooled.copiedBuffer(convertToJson(responseStructure.getRestFulResponse().response(o)), CharsetUtil.UTF_8)
+            );
+            responseStructure.getCookies().forEach(cookie-> {
+                StringBuilder builder = new StringBuilder();
+                builder.append(cookie.getCookieName() + "=" + cookie.getCookieValue() + ";");
+                cookie.getFeatures().forEach(cookieFeature -> builder.append(" " + cookieFeature.toValue() + ";"));
+                response.headers().add(HttpHeaderNames.SET_COOKIE, builder.toString());
+            });
+
+            responseStructure.getRestFulResponse().initializeSettings(
+                    new RestFulResponseHelper(service.getCtx(), structure, responseStructure,
+                            response, receivedCookies));
+
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "application/json");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
+            service.getCtx().writeAndFlush(response);
+        } catch (Exception e) {
+            FullHttpResponse response = new DefaultFullHttpResponse(
+                    HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST,
+                    Unpooled.copiedBuffer("", CharsetUtil.UTF_8)
+            );
+
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "application/json");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
+            service.getCtx().writeAndFlush(response);
+        }
+
+
+    }
+
+
+    private String convertToJson(Object object) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(object);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error converting to JSON";
+        }
+    }
+}
